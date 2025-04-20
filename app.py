71029130b44080446d9ca8826c81846620c002d7
JeBoiBuurman <swederhaarmans13@gmail.com>
2025-04-20 22:25:37 +0200

    updated google sheets update system

--- app.py
+++ app.py
@@ -60,33 +60,33 @@ new_df = pd.DataFrame([dataframe])
 if st.button("Save"):
     if not username:
         st.warning("Please enter your username before saving.")
-    else:
-        user_file = f"{username}_mood_data.csv"
-        master_file = "data/all_user_mood_data.csv"
+    #else:
+     #   user_file = f"{username}_mood_data.csv"
+    #    master_file = "data/all_user_mood_data.csv"
 
         #save to user file
-        if os.path.exists(user_file):
-            df = pd.read_csv(user_file)
-            df = pd.concat([df, new_df], ignore_index=True)
-        else:
-            df = new_df
-
-        df.drop_duplicates(subset=["Date"], keep="last", inplace=True)
-        df.to_csv(user_file, index=False)
-
-        # save to master file
-        if os.path.exists(master_file):
-            master_df = pd.read_csv(master_file)
-            master_df = pd.concat([master_df, new_df], ignore_index=True)
-        else:
-            master_df = new_df
+    #    if os.path.exists(user_file):
+    #        df = pd.read_csv(user_file)
+    #        df = pd.concat([df, new_df], ignore_index=True)
+    #    else:
+    #        df = new_df
+#
+ #       df.drop_duplicates(subset=["Date"], keep="last", inplace=True)
+ #       df.to_csv(user_file, index=False)
+#
+#        # save to master file
+#        if os.path.exists(master_file):
+#            master_df = pd.read_csv(master_file)
+#            master_df = pd.concat([master_df, new_df], ignore_index=True)
+#        else:
+#            master_df = new_df
 
         
         
-        master_df.drop_duplicates(subset=["Date"], keep='last', inplace=True)
-        master_df.to_csv(master_file, index=False)
+ #       master_df.drop_duplicates(subset=["Date"], keep='last', inplace=True)
+  #      master_df.to_csv(master_file, index=False)
         
         google_data = [username, str(datum), mood, energy, stress, sleep, note]
-
+        sheet.append_row(google_data)
         
         st.success("Your entry has been saved!")
